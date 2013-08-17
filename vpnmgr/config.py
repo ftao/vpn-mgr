5e92ef2e3ed3bd8d356923b63d30be755b6d579a
ftao <Filia.Tao@gmail.com>
2013-08-17 12:47:36 +0800

    add basic api key auth

--- vpnmgr/config.py
+++ vpnmgr/config.py
@@ -1,5 +1,3 @@
+import os
 OPENVPN_CONFIG_DIR = '/etc/openvpn/'
-
-
-
-
+API_KEY = os.environ.get('VPNGMR_WEB_API_KEY', 'default-vpnmgr-key-b80a')
--- vpnmgr/webapi.py
+++ vpnmgr/webapi.py
@@ -3,11 +3,20 @@
 import web
 import json
 from vpnmgr.common import list_users, kick_user
+from vpnmgr import config
 
 urls = (
   '/session/list\.(.*)', "list_sessions",
   '/session/disconnect/(.+)', "disconnect_session",
 )
+
+def auth_processor(handler): 
+    query = web.input()
+    k = query.get('k', '')
+    if k != config.API_KEY:
+        return web.Forbidden()
+    else:
+        return handler()
 
 class list_sessions:
 
@@ -56,7 +65,7 @@ class disconnect_session:
         return {"ok" : kick_user(username)}
 
 app = web.application(urls, globals())
-#app.add_processor(auth_processor)
+app.add_processor(auth_processor)
 
 def main():
     app.run()
